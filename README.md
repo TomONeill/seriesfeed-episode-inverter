aea6f12b9e061a95981243cbb5155e232f0c5c7b
Tom O'Neill <TomONeill@users.noreply.github.com>
2017-10-03 23:29:07 +0200

    Added v1.0.

---
+++ README.md
@@ -0,0 +1,29 @@
+# Seriesfeed Episode Inverter
+Allows you to invert the episode list on a series.
+<BR/>
+Enjoy.
+<BR/><BR/>
+Version <strong>1.0</strong>
+
+<A HREF="https://github.com/TomONeill/seriesfeed-episode-inverter/raw/master/seriesfeed-episode-inverter-latest.user.js">INSTALL</A>
+
+# Screenshots
+Comparison between the original and with the userscript.<BR />
+<img src="https://raw.githubusercontent.com/TomONeill/seriesfeed-episode-inverter/master/screenshots/watchlist.png"  width="250px" />
+<img src="https://raw.githubusercontent.com/TomONeill/seriesfeed-episode-inverter/master/screenshots/episodes.png" width="250px" />
+
+# Changelog
+<A HREF="https://raw.githubusercontent.com/TomONeill/seriesfeed-episode-inverter/master/changelog.txt">View changelog</A>
+
+# Other userscripts
+Check out other userscripts for Seriesfeed by me:<BR/>
+<ul>
+    <li><A HREF="https://github.com/TomONeill/Seriesfeed-Importer">Import favourites</A></li>
+    <li><A HREF="https://github.com/TomONeill/Seriesfeed-Import-Time-Wasted">Import Time Wasted</A></li>
+    <li><A HREF="https://github.com/TomONeill/Seriesfeed-Move">Move cards on frontpage</A></li>
+    <li><A HREF="https://github.com/TomONeill/Seriesfeed-Season-Separator">Season Separator</A></li>
+</ul>
+
+# Donate
+If you like my work so much you feel like doing something nice for me, a complete stranger of the internet, you can.<BR />
+<A HREF="https://www.paypal.me/TomONeill">Donate here</A>.
---
+++ seriesfeed-episode-inverter-latest.user.js
@@ -0,0 +1,89 @@
+﻿// ==UserScript==
+// @name         Seriesfeed Episode Inverter
+// @namespace    https://www.seriesfeed.com
+// @version      1.0
+// @description  Allows you to invert the episode list on a series.
+// @updateURL 	 https://github.com/TomONeill/seriesfeed-episode-inverter/raw/master/seriesfeed-episode-inverter-latest.user.js
+// @match        https://*.seriesfeed.com/**/episodes
+// @match        https://*.seriesfeed.com/**/episodes/*
+// @match        https://www.seriesfeed.com/series/schedule/history
+// @grant        unsafeWindow
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @require      https://code.jquery.com/jquery-3.2.1.min.js
+// @author       Tom
+// @copyright    2017+, Tom
+// ==/UserScript==
+/* jshint -W097 */
+/*global $, console, GM_getValue, GM_setValue */
+'use strict';
+
+$(function() {
+	let sortButton;
+
+	if (window.location.href.indexOf("episodes") > -1) {
+		const onClick = () => {
+			const shouldDescend = GM_getValue("seriesShouldDescend");
+			GM_setValue("seriesShouldDescend", !shouldDescend);
+			sortEpisodes(!shouldDescend);
+		};
+		sortButton = buttonFactory(onClick);
+		$(".container .row .col-xs-12.col-sm-6.col-md-4").prepend(sortButton);
+
+		const shouldDescend = GM_getValue("seriesShouldDescend");
+		if (shouldDescend) {
+			invertEpisodes();
+			updateButton(sortButton, 'up', "aflopend");
+		} else {
+			updateButton(sortButton, 'down', "oplopend");
+		}
+	}
+
+	function sortEpisodes(shouldDescend) {
+		invertEpisodes();
+
+		if (shouldDescend) {
+			updateButton(sortButton, 'up', "aflopend");
+		} else {
+			updateButton(sortButton, 'down', "oplopend");
+		}
+	}
+
+	function invertEpisodes() {
+		const episodes = $('#afleveringen tbody');
+		episodes.each(function(element, index) {
+			const array = $.makeArray($("tr", this).detach());
+			array.reverse();
+			$(this).append(array);
+		});
+	}
+
+	function updateButton(button, anglePosition, text) {
+		button.html('<i class="fa fa-angle-' + anglePosition + '"></i>' + text);
+	}
+
+	function buttonFactory(onClick) {
+		const button = $('<button class="btn btn-default"></button>');
+		button.on('click', onClick);
+
+		return button;
+	}
+
+	if (window.location.href.indexOf("series/schedule") > -1) {
+		const onClick = () => {
+			const shouldAscend = GM_getValue("watchlistShouldAscend");
+			GM_setValue("watchlistShouldAscend", !shouldAscend);
+			sortEpisodes(shouldAscend);
+		};
+		sortButton = buttonFactory(onClick);
+		$(".container .rightButtons").prepend(sortButton);
+
+		const watchlistShouldAscend = GM_getValue("watchlistShouldAscend");
+		if (watchlistShouldAscend) {
+			invertEpisodes();
+			updateButton(sortButton, 'down', "oplopend");
+		} else {
+			updateButton(sortButton, 'up', "aflopend");
+		}
+	}
+});
